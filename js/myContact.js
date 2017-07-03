8cd447cbdfdc2432e3b141f3dab40e3715b4b04c
Mohanad Elhag <moh.7afiz@gmail.com>
2017-07-03 08:44:39 +0300

    added js file

---
+++ js/myContact.js
@@ -0,0 +1,8 @@
+// Get input element
+let filterInput = document.getElementById(filterInput);
+// Add event listener
+filterInput.addEventListener('keyup', filterNames); 
+
+function filterNames() {
+    console.log(1);
+}
